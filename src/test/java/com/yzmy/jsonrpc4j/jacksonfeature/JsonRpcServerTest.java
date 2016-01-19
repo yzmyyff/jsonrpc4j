9e507e4c17a3f03ea9236c5cf451e2c3ad3c4d10
yzmyyff <rukuang10@gmail.com>
2016-01-19 15:01:21 +0800

    写了符合预期的单元测试

--- src/test/java/com/yzmy/jsonrpc4j/jacksonfeature/JsonRpcServerTest.java
+++ src/test/java/com/yzmy/jsonrpc4j/jacksonfeature/JsonRpcServerTest.java
@@ -1,14 +1,35 @@
 package com.yzmy.jsonrpc4j.jacksonfeature;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.yzmy.jsonrpc4j.JsonRpcBasicServer;
+import org.junit.Assert;
 import org.junit.Test;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 
 /**
  * 主要用于测试是否可以正常使用一些jackson的特性.
  * Created by Kuang.Ru on 2016/1/18.
  */
 public class JsonRpcServerTest {
+    /**
+     * 测试能否正常使用Jackson的反序列化特性
+     */
     @Test
     public void testDeserializeFeature() {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.enable(DeserializationFeature.FAIL_ON_NULL_FOR_PRIMITIVES);
+        JsonRpcBasicServer jsonRpcServer = new JsonRpcBasicServer(mapper, new Service(), Service.class);
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
 
+        try {
+            jsonRpcServer.handle(new ClassPathResource("jackson/jacksonDeserialize.json").getInputStream(), baos);
+            Assert.fail("必须得抛个错.");
+        } catch (Exception e) {
+            // 没抛就是错的
+        }
     }
 }
---
+++ src/test/java/com/yzmy/jsonrpc4j/jacksonfeature/Service.java
@@ -0,0 +1,15 @@
+package com.yzmy.jsonrpc4j.jacksonfeature;
+
+import com.yzmy.jsonrpc4j.JsonRpcMethod;
+import com.yzmy.jsonrpc4j.JsonRpcParam;
+
+/**
+ * jackson特性测试类
+ * Created by Kuang.Ru on 2016/1/19.
+ */
+public class Service {
+    @JsonRpcMethod("testPrimitiveFailFeature")
+    public String testPrimitiveFailFeature(@JsonRpcParam("id") int id) {
+        return "success " + id;
+    }
+}
